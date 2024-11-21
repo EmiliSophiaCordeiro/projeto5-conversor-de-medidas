9f9b698ba8494211b00e43f3d59bf6ad54f4c203
Emili Sophia Cordeiro <emilisophiassss@gmail.com>
2024-11-21 09:29:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-function converterMedida(metros, opcao) {
-    switch (opcao) {
-        case 'mm':
-            return metros * 1000;
-        case 'cm':
-            return metros * 100;
-        case 'dm':
-            return metros * 10;
-        case 'dam':
-            return metros / 10;
-        case 'hm':
-            return metros / 100;
-        case 'km':
-            return metros / 1000;
-        default:
-            return 'Opção inválida';
-    }
+// Captura os dados do usuário
+let valorEmMetros = parseFloat(prompt("Digite um valor em metros:"));
+let unidadeDesejada = prompt("Escolha a unidade de medida (mm, cm, dm, dam, hm ou km):");
+
+// Verifica qual a conversão a ser feita com base na unidade desejada
+let resultado;
+
+switch (unidadeDesejada) {
+    case 'mm':
+        resultado = valorEmMetros * 1000;
+        break;
+    case 'cm':
+        resultado = valorEmMetros * 100;
+        break;
+    case 'dm':
+        resultado = valorEmMetros * 10;
+        break;
+    case 'dam':
+        resultado = valorEmMetros / 10;
+        break;
+    case 'hm':
+        resultado = valorEmMetros / 100;
+        break;
+    case 'km':
+        resultado = valorEmMetros / 1000;
+        break;
+    default:
+        console.log('Opção inválida');
+        break;
 }
 
-const valorEmMetros = parseFloat('Digite um valor em metros:');
-
-const unidadeDesejada = 'Escolha a unidade de medida (mm, cm, dm, dam, hm ou km):';
-
-const resultado = converterMedida(valorEmMetros, unidadeDesejada);
-
-if (typeof resultado === 'number') {
+// Exibe o resultado, caso a opção seja válida
+if (resultado !== undefined) {
     console.log(`Resultado: ${resultado} ${unidadeDesejada}`);
-} else {
-    console.log(resultado);
 }
+
